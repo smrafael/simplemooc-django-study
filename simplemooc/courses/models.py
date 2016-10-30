35387261e747a939dacd2ab211e55e04ad2d5697
Rafael <rafael.sm1990@gmail.com>
2016-10-30 13:51:16 -0300

    customizing admin

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 
-class Course(models.Model):
+class CourseManager(models.Manager):
 
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) |
+            models.Q(description__icontains=query)
+        )
+
+
+class Course(models.Model):
     name = models.CharField('Nome', max_length=100)
     slug = models.SlugField('Atalho')
     description = models.TextField('Descrição', blank=True)
@@ -11,3 +19,12 @@ class Course(models.Model):
                               null=True, blank=True)
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
