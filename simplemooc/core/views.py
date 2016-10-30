48efa7b5b78c5ff512a7c1e5227ef952c4165e11
Rafael <rafael.sm1990@gmail.com>
2016-10-29 21:15:09 -0300

    adding requiments.txt, adding template and adding urls.py to core app

--- simplemooc/core/views.py
+++ simplemooc/core/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'home.html', {'usuario': 'Fulano de tal'})
+    return render(request, 'home.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
