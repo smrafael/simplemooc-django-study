ca3f3fe8cc254d7bf124fae6b73415855f957b13
Rafael <rafael.sm1990@gmail.com>
2016-10-30 23:01:44 -0300

    course view

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Course
 
-# Create your views here.
+
+def index(request):
+    template_name = 'courses/index.html'
+    context = {
+        'courses': Course.objects.all()
+    }
+    return render(request, template_name, context)
+
+
+def details(request, pk):
+    template_name = 'courses/details.html'
+    context = {
+        'course': get_object_or_404(Course, pk=pk)
+    }
+    return render(request, template_name, context)
