9f04686407c10693044b88ac153d4088cd7f0d1d
rafael.mendes <rafael.mendes@embedded.ufcg.edu.br>
2016-11-01 17:29:46 -0300

    get via slug and links on listing

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -10,9 +10,16 @@ def index(request):
     return render(request, template_name, context)
 
 
-def details(request, pk):
+# def details(request, pk):
+#     template_name = 'courses/details.html'
+#     context = {
+#         'course': get_object_or_404(Course, pk=pk)
+#     }
+#     return render(request, template_name, context)
+
+def details(request, slug):
     template_name = 'courses/details.html'
     context = {
-        'course': get_object_or_404(Course, pk=pk)
+        'course': get_object_or_404(Course, slug=slug)
     }
     return render(request, template_name, context)
